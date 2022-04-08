acdcb3a598d5e3bb31be4a39f8ffbbd5c2073b0b
AlifioDitya <alifioditya@gmail.com>
2022-04-09 01:41:20 +0700

    Function F04 in progress

---
+++ Program/F04.py
@@ -0,0 +1,17 @@
+import functions as func
+
+def addGame(dataMtx):
+    name = input("Masukkan nama game: ")
+    ctgry = input("Masukkan kategori: ")
+    year = input("Masukkan tahun rilis: ")
+    price = input("Masukkan harga: ")
+    stock = input("Masukkan stok awal: ")
+    while (name == "" or ctgry == "" or year == "" or price == "" or stock == ""):
+        print("Mohon masukkan semua informasi mengenai game agar dapat disimpan BNMO.")
+        name = input("Masukkan nama game: ")
+        ctgry = input("Masukkan kategori: ")
+        year = input("Masukkan tahun rilis: ")
+        price = input("Masukkan harga: ")
+        stock = input("Masukkan stok awal: ")
+    updated = dataMtx + [[name, ctgry, year, price, stock]]
+    return updated
