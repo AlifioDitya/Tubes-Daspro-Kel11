4c57e447e848f96c8a319e734ef30dc3b4a3c255
AlifioDitya <alifioditya@gmail.com>
2022-04-13 19:26:02 +0700

    Fixed F16

--- Program/F16.py
+++ Program/F16.py
@@ -2,14 +2,14 @@ import functions as func
 
 def mtxToCSV(mtx):
 # Prosedur mengubah bentuk data dari matriks ke string terpisah oleh titik koma
-    temp = " "
+    temp = ""
     for line in mtx:
         for element in line:
             if element != line[func.length(line)-1]:
                 temp += str(element) + ";"
             else: temp += str(element) + "\n"
     return temp
-        
+
 def save(matrix, file):
 # Prosedur melakukan penulisan data berbentuk matriks ke dalam file csv tujuan
     f = open(file, "w")
