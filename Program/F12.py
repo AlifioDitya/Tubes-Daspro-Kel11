14a71f05f43242dfa866df45ce30453d026c92c1
AlifioDitya <alifioditya@gmail.com>
2022-04-13 20:25:55 +0700

    changed F12 to rama's version

--- Program/F12.py
+++ Program/F12.py
@@ -1,15 +1,18 @@
 import functions as func
 
-def topup(mtxUserData):
+def topup(dataMtx):
 # Prosedur melakukan topup saldo untuk user tertentu
     username = input("Masukkan username: ")
-    while not func.isInData(mtxUserData, username, 1):
-        print("Username \"" + str(username) + "\" tidak ditemukan." )
+    CekUsername = func.isInData(dataMtx,username,1)
+    while not CekUsername:
+        print("Username “" + username + "” tidak ditemukan.")
         username = input("Masukkan username: ")
-    idx = func.retrieveIdx(mtxUserData, username, 1)
-    addSaldo = int(input("Masukkan saldo: "))
-    while (addSaldo + int(mtxUserData[idx][5])) < 0:
+    idx = func.retrieveIdx(dataMtx,username,1)
+    tambah = int(input("Masukkan saldo: "))
+    while (int(dataMtx[idx][5]) + tambah) < 0:
         print("Masukan tidak valid.")
-        addSaldo = int(input("Masukkan saldo: "))
-    mtxUserData[idx][5] = str(addSaldo + int(mtxUserData[idx][5]))
+        tambah = int(input("Masukkan saldo: "))
+    dataMtx[idx][5] = str(int(dataMtx[idx][5]) + tambah)
+    nama = dataMtx[idx][2]
+    print("Top up berhasil. Saldo " + nama + " bertambah menjadi " + str(dataMtx[idx][5]) + ".")
     return
