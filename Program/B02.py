b9c3b4dc1980f41f7052c6ccbf71bf3ebcac355c
AlifioDitya <alifioditya@gmail.com>
2022-04-13 23:43:05 +0700

    Added magic conch shell (B02)

---
+++ Program/B02.py
@@ -0,0 +1,29 @@
+from datetime import datetime as dt
+
+def lcg():
+# Prosedur menghasilkan angka dalam range [0..6] secara pseudorandom 
+# dengan nilai seed berupa milisecond waktu sekarang
+# Nilai a dan b pada algoritma lcg ini mengikuti yang digunakan oleh Microsoft Visual C++
+    x = int(dt.now().strftime("%f"))
+    a = 214013
+    b = 2531011
+    return ((a*x + b)%7)
+
+def mcs():
+# Prosedur magic conch shell memberikan jawaban secara pseudorandom
+    a = input("Apa pertanyaanmu? ")
+    n = lcg()
+    if n == 0:
+        print("Iya.")
+    elif n == 1:
+        print("Tidak.")
+    elif n == 2:
+        print("Mungkin.")
+    elif n == 3:
+        print("Bisa jadi.")
+    elif n == 4:
+        print("Tergantung.")
+    elif n == 5:
+        print("Belum tentu.")
+    elif n == 6:
+        print("Ya ndak tau kok tanya saya.")
