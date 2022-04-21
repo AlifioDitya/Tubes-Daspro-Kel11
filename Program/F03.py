c3c9f8b40b360ecbc8c6122d062c70b76b902da1
AlifioDitya <alifioditya@gmail.com>
2022-04-21 13:40:44 +0700

    Added ASCII Art on login

--- Program/F03.py
+++ Program/F03.py
@@ -1,5 +1,5 @@
-import functions as func
 from B01 import decipher
+import functions as func
 
 def login(userMtx):
 # Fungsi untuk login pengguna saat awal program dimulai, mengembalikan informasi dari pengguna yang login.
@@ -20,8 +20,30 @@ def login(userMtx):
         print()
     name = userMtx[func.retrieveIdx(userMtx, username, 1)][2]
     func.loading("Logging in")
-    print()
+    print('''
+
+ █───█ █▀▀▀ █─── █▀▀▀ █▀▀▀█ █▀▄▀█ █▀▀▀ 
+ █▄█▄█ █▀▀▀ █─── █─── █───█ █─▀─█ █▀▀▀ 
+  ▀ ▀  ▀▀▀▀ ▀▀▀▀ ▀▀▀▀ ▀▀▀▀▀ ▀   ▀ ▀▀▀▀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡏⠱⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡠⠤⠤⣄⠀⠀⠀⠀⠀⡇⢰⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠤⠖⠂⣉⣠⠤⠤⢤⡈⢆⠀⠀⠀⢸⠁⠘⠀⠀⠀
+    ⠀⠀⠀⠀⠀⣀⠤⠒⣈⣡⠤⠔⠛⠉⠁⠀⠀⠀⠀⢧⠈⡄⠀⠀⡞⣰⡆⠀⠀⠀
+    ⠀⠀⠀⠰⢁⡴⠞⠋⠁⠀⠀⠀⠀⠀⢀⠴⠛⠓⠂⠘⡆⠰⡀⡜⢱⡿⠀⠀⠀⠀
+    ⠀⠀⠀⢰⣼⡇⠀⠀⡠⠶⠶⠂⠀⠀⠀⠀⠀⠀⠀⠀⢹⠀⢿⣧⡿⠃⠀⠀⠀⠀
+    ⠀⠀⠀⠈⣿⣿⡀⠈⠀⠀⠀⢠⣴⣶⣿⣿⡇⠀⠀⠀⠀⡇⠘⡟⠁⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⢸⣿⣧⠀⠀⠀⠀⠘⢿⡟⣛⡻⣃⣠⣴⡶⠟⠀⠀⢰⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⢫⢻⣆⠀⣀⣀⣠⣤⣶⡿⠿⠛⠋⠁⢰⣿⡆⠀⠀⡇⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⢈⣎⠙⠛⠛⢛⣫⣽⣧⡀⠀⠀⠀⠀⠈⠛⠃⠀⠀⠸⡀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⡴⣹⡿⡔⣶⣿⣿⠿⠛⠋⠁⠀⠀⠀⣿⠳⡄⢀⣤⡄⠀⢧⠀⠀⠀⠀
+    ⠀⠀⠀⡘⢠⡟⠀⢳⠉⢱⡏⠉⠣⠒⢲⠀⠀⠀⠯⢾⣭⣼⡿⠷⠀⠘⡀⠀⠀⠀
+    ⠀⠀⠀⠁⠉⠀⠀⠀⠁⠉⠁⠀⠀⠀⠉⠀⠀⠀⠀⠈⠉⠉⠁⠀⠀⠀⠁⠀⠀⠀
+    '''
+    )
     print("Halo " + name + "! Selamat datang di Binomo.")
+    print()
+    input("Klik enter untuk memasuki main menu." )
     userID = userMtx[func.retrieveIdx(userMtx, username, 1)][0]
     userRole = userMtx[func.retrieveIdx(userMtx, username, 1)][4]
     return (userID, userRole)  # informasi pengguna yang sedang mengoperasikan sistem
