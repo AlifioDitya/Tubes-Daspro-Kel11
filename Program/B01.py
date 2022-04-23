5990f00749a9172014dca1c82c409afb1f4ad98b
AlifioDitya <alifioditya@gmail.com>
2022-04-23 16:46:07 +0700

    Added documentations to B01 and F14

--- Program/B01.py
+++ Program/B01.py
@@ -1,19 +1,36 @@
+# File Bonus Cipher
+# Kontributor : Enrique Alifio Ditya/16521253
+
 import functions as func
 
 def findKeystream (plaintext, key):
-# Fungsi mengenerate keystream untuk dipakai pada proses ciphering/deciphering pesan
+# Fungsi menghasilkan keystream untuk dipakai pada proses ciphering/deciphering pesan
+# Menerima masukan suatu plaintext dan keyword, kemudian menghasilkan perulangan keyword sampai sepanjang plaintext
+# Contoh: input : plaintext "helloworld" ; key "hola" ; output : "holaholaho"
+# KAMUS LOKAL
+#   keystream : string
+#   i : integer
+# ALGORITMA
     keystream = ""
     if func.length(plaintext) < func.length(key):
         for i in range(func.length(plaintext)):
             keystream += key[i]
     else:
         keystream += key
-        keystream += findKeystream (plaintext[func.length(keystream):], key)
+        keystream += findKeystream (plaintext[func.length(keystream):], key)   # rekursi apabila plaintext lebih panjang daripada key
     return keystream
 
 def cipher(plaintext):
-# Fungsi mengembalikan pesan tersembunyi dengan algoritma keyed vigenere cipher
-# Diasumsikan string masukan tidak akan memiliki karakter selain yang terdapat pada variabel characters di subprogram ini
+# Fungsi mengembalikan pesan tersembunyi dengan algoritma keyed vigenere cipher.
+# Diasumsikan string masukan tidak akan memiliki karakter selain yang terdapat pada variabel characters di subprogram ini.
+# Algoritma keyed vigenere cipher merupakan cipher berjenis poly alphabetic substitution. 
+# Array of characters ditabulasikan kemudian plaintext dan keystream masing-masing akan menentukan kolum serta baris pemetaan dalam tabel karakter tersebut.
+# Lebih jelasnya dapat dilihat di link berikut: https://youtu.be/SkJcmCaHqS0
+# Credit to Udacity on YouTube.
+# KAMUS
+#   characters, key, keystream, shiftedC, ciphertext : string
+#   i, j, k, x, y, idx : integer
+# ALGORITMA
     characters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789!@#$%^&*()-_=+:/"
     key = "Vigenere16521253"
     keystream = findKeystream(plaintext, key)
@@ -21,6 +38,7 @@ def cipher(plaintext):
     ciphertext = ""
     for i in range(func.length(plaintext)):
         for j in range(func.length(characters)):
+            # Menenutkan baris character untuk pemetaan
             if characters[j] == plaintext[i]:
                 shiftedC = ""
                 for x in range(j, func.length(characters)):
@@ -37,7 +55,12 @@ def cipher(plaintext):
     return ciphertext
 
 def decipher (ciphertext, key):
-# Fungsi melakukan deciphering pesan tersembunyi oleh algoritma keyed vigenere
+# Fungsi melakukan deciphering pesan tersembunyi oleh algoritma keyed vigenere.
+# Merupakan hasil reverse-engineering dari subprogram cipher.
+# KAMUS
+#   characters, keystream, shiftedR, plaintext : string
+#   i, j, k, x, y, idx : integer
+# ALGORITMA
     characters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789!@#$%^&*()-_=+:/"
     keystream = findKeystream(ciphertext, key)
     shiftedR = ""
